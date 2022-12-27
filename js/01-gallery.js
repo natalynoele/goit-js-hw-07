721072770a89d4ffcaebf6fd33d33de273cc76b8
natalynoele <natalynoele@gmail.com>
2022-12-27 22:47:55 +0200

    added gallery markup task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
 
+
+function createGalleryMarkup(imagesCollections) {
+  return imagesCollections
+    .map(
+      ({ description, original, preview }) =>
+        `<li class="gallery__item"><img class="gallery__image" src="${preview}" alt="${description}"></li>`
+    )
+    .join("");
+}
+
+function renderGalleryMarkup() {
+  if (galleryContainer) {
+    const galleryImages = createGalleryMarkup(galleryItems);
+    galleryContainer.insertAdjacentHTML("beforeend", galleryImages);
+  }
+}
+renderGalleryMarkup();
 console.log(galleryItems);
