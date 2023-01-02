bf9acbd96c2da75e7576ba930238b35fcdc03bff
natalynoele <natalynoele@gmail.com>
2023-01-03 00:10:10 +0200

    finished task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,22 +1,51 @@
 import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 const galleryContainer = document.querySelector(".gallery");
-
+const galleryImages = createGalleryMarkup(galleryItems);
 
 function createGalleryMarkup(imagesCollections) {
   return imagesCollections
     .map(
       ({ description, original, preview }) =>
-        `<li class="gallery__item"><img class="gallery__image" src="${preview}" alt="${description}"></li>`
+        `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="Image description"
+          />
+        </a>
+      </div>`
     )
     .join("");
 }
 
-function renderGalleryMarkup() {
-  if (galleryContainer) {
-    const galleryImages = createGalleryMarkup(galleryItems);
-    galleryContainer.insertAdjacentHTML("beforeend", galleryImages);
-  }
+function renderGalleryMarkup(container, markup) {     
+    container.insertAdjacentHTML("beforeend", markup);  
 }
-renderGalleryMarkup();
+
+renderGalleryMarkup(galleryContainer,galleryImages);
+
+galleryContainer.addEventListener('click', onGalleryImageClick);
+
+function onGalleryImageClick(evt){  
+    evt.preventDefault();
+    if(evt.target.nodeName !== 'IMG'){
+      return;
+    }
+     const instance = basicLightbox.create(`
+      <img src="${evt.target.dataset.source}">
+  `).show();
+ 
+  }
+
+document.addEventListener('keydown', evt => {
+  if (evt.key === 'Escape') {
+    const instance = document.querySelector('.basicLightbox--visible');
+    instance.classList.remove('basicLightbox--visible');
+    instance.classList.add('basicLightbox--invisible')
+  }
+});
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+const galleryImages = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(imagesCollections) {
+  return imagesCollections
+    .map(
+      ({ description, original, preview }) =>
+        `<li><a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a></li>`
+    )
+    .join("");
+}
+
+function renderGalleryMarkup(container, markup) {
+  container.insertAdjacentHTML("beforeend", markup);
+}
+
+renderGalleryMarkup(galleryContainer, galleryImages);
 
 console.log(galleryItems);
